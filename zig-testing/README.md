ea141084652c429aebb76f915ed3a57e7b67ffb5
Allison Durham <AdjetiveAllison@gmail.com>
2023-05-26 22:21:53 -0500

    zig is badass

---
+++ zig-testing/README.md
@@ -0,0 +1,5 @@
+# ZIG is a super cool language and here I am learning it.
+
+## https://ziglearn.org/
+
+This commit is going to be pushed when I am midway through chapter 1,  I just got to "optionals". I streamed myself looking at the learning docs.
---
+++ zig-testing/main.zig
@@ -0,0 +1,112 @@
+const std = @import("std");
+const Value = enum(u2) { zero, one, two };
+
+const MyCoolStruct = struct { q: f32, l: [5]u8, d: i32 };
+
+pub fn main() void {
+    std.debug.print("Hello, {s}!\n", .{"World"});
+
+    const blah = [_]u8{ 'a', 'A', 'z', 'Z' };
+
+    std.debug.print("{s}\n", .{blah});
+    // std.debug.print(&blah, .{}); does the same thing.
+
+    const length = blah.len;
+    std.debug.print("{d}\n", .{length});
+
+    // x += if (a) 1 else 2; -- if statements can be used kinda similar to elvis statements in other languages.
+
+    //const timer = std.time.Timer;
+    //timer.start()
+    // var i: u8 = 0;
+    // while (i < 100) {
+    //     //const loopTime = timer.lap();
+    //     i += 1; // zig has no ++
+    //     //std.debug.print("Loop took {d} nano seconds to run.", .{loopTime});
+    //     std.debug.print("{d}\n", .{i});
+    //     //if (i == 72) break; -- this short hand is phenomenal.
+    //     //break
+
+    // }
+
+    std.debug.print("{u}\n", .{96});
+    for (blah, 0..) |character, index| {
+        // below has formatting types for printing interpolation. u/c(utf8/ascii) work for u8 characters and s works for slices of u8.
+        // https://github.com/ziglang/zig/blob/master/lib/std/fmt.zig
+        std.debug.print("{u} character is at {d} index in string with {d} length\n", .{ character, index, length });
+    }
+
+    //this is unbelievably fast and cool.
+    const x = fibonacci(38);
+
+    std.debug.print("{d}\n", .{x});
+
+    deferWeirdness();
+
+    // crucial concept of errors:
+    // try x is a shortcut for x catch |err| return err
+
+    // switches are strange and you need to understand the difference between statement and expression.
+    // I believe expression just modifies the input to the switch directly?
+
+    if (1 == 3) {
+        // I can force compiler to be angry if somewhere in code is reached and I didn't want it to ever be able to be reached. I fuckin love this :)
+        unreachable;
+    }
+
+    std.debug.print("enum value 'two' is {d} int\n", .{@enumToInt(Value.two)});
+
+    const superCoolStruct = MyCoolStruct{
+        .d = 15,
+        .l = [5]u8{ 'h', 'e', 'l', 'l', 'o' }, // Trying to do this as a slice left me having errors with syntax and pointers. I believe my struct did have a properly defined slice, but this was wrong.
+        .q = 161,
+    };
+
+    std.debug.print("{}\n", .{superCoolStruct});
+
+    // _ can be used as a visual seperator in integers.
+    const oneBillion: u64 = 1_000_000_000;
+    const permissions: u64 = 0o7_5_5; // octal
+
+    std.debug.print("1_000_000_000 = {}\n", .{oneBillion});
+    std.debug.print("0o7_5_5 = {}\n", .{permissions});
+
+    // convert integers to a new type - illegal behavior if out of range.
+    //@intCast(comptime DestType: type, int: anytype)
+
+    //overflow wrapping operators are interesting.
+    //var a: u8 = 255;
+    //a +%= 1;
+    //try expect(a == 0);
+
+    // intToFloat is always legal
+    //@intToFloat(comptime DestType: type, int: anytype)
+    // floatToInt can be illegal if not a whole number.
+    //@floatToInt(comptime DestType: type, float: anytype)
+
+    // loops as expressions and breaking/continuing to labels is some powerful mumbo jumbo.
+
+}
+
+fn deferWeirdness() void {
+    var x: f32 = 5;
+    {
+        defer x += 2;
+        defer x /= 2;
+    }
+    std.debug.print("multiple defers execute in reverse order.\n", .{});
+    std.debug.print("That means 5 / 2 = 2.5 and 2.5 + 2 = 4.5 -- {any}\n", .{x == 4.5});
+    // it looks like {} and {any} are the same thing for string fmt.
+}
+
+//u32 so big
+fn fibonacci(n: u32) u32 {
+    if (n == 0 or n == 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+// fn testingForFun(stringThing: []u8, extendBy: u32) []u8 {
+//     _ = extendBy;
+//     const stringLength = stringThing.len;
+//     _ = stringLength;
+// }
